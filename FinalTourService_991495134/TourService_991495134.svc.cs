2f30adf8f4c1e04607dcaf29925c330152cbaf02
Jaesung <me@jaesunglee.ca>
2020-12-18 14:58:21 -0500

    Added TourService

---
+++ FinalTourService_991495134/TourService_991495134.svc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace FinalTourService_991495134
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "TourService_991495134" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select TourService_991495134.svc or TourService_991495134.svc.cs at the Solution Explorer and start debugging.
+    public class TourService_991495134 : ITourService_991495134
+    {
+        public void DoWork()
+        {
+        }
+    }
+}
